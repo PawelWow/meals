09bc293d7dc0de1ebf43516a3f92c67f337f5792
pawel <pawel@webperfect.pl>
2020-08-15 18:56:06 +0200

    choosing meal categorie - navigate to category meals screen with specific category data.

--- components/screens/CategoryMealsScreen.js
+++ components/screens/CategoryMealsScreen.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Platform } from 'react-native';
+
+import { CATEGORIES } from '../../data/dummy-data';
+import Colors from '../../constans/Colors';
 
 const CategoryMealsScreen = props => {
-    return(
+    const catId = props.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id == catId);
+
+    return(        
         <View style={styles.screen}>
             <Text>Category Meals Screen</Text>
-            <Button title="GO TO MEAL Detail" onPress={() => {
+            <Text>{selectedCategory.title}</Text>
+            <Button title="Go to Details" onPress={() => {
                 props.navigation.navigate('MealDetail');
             }} />
+            <Button title="Go Back" onPress={ () => { props.navigation.pop(); }} />
         </View>
     );
+};
+
+CategoryMealsScreen.navigationOptions = navigationData => {
+    const catId = navigationData.navigation.getParam('categoryId');
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+    return {
+        headerTitle: selectedCategory.title,
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    };
 };
 
 const styles = StyleSheet.create({
