7cca987ee15598036e8b4533a30b746cffc01bc9
pawel <pawel@webperfect.pl>
2020-08-16 13:02:12 +0200

    adding fonts to navigation bars

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Platform } from 'react-native';
+import { Text, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import {createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs'
@@ -18,6 +18,12 @@ import Colors from '../constans/Colors';
 const defaultStackNavOptions = {
     headerStyle: {
         backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTitleStyle: {
+        fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+        fontFamily: 'open-sans'
     },
     headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
     headerTitle: 'A Screen'
@@ -53,17 +59,18 @@ const tabSScreenConfig = {
             tabBarIcon: (tabInfo) => {
                 return ( <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />);
             },
-            tabBarColor: Colors.primaryColor
+            tabBarColor: Colors.primaryColor,
+            tabBarLabel: Platform.OS === 'android' ? (<Text style={{ fontFamily: 'open-sans-bold'}}>Meals</Text>) : ('Meals')
         }
     },
     Favorites: {
         screen: FavNavigator,
         navigationOptions: {
-            tabBarLabel: 'Favorites!',
             tabBarIcon: tabInfo => {
                 return ( <Ionicons name="ios-star" size={25} color={tabInfo.tintColor}/> );
             },
-            tabBarColor: Colors.accentColor
+            tabBarColor: Colors.accentColor,
+            tabBarLabel: Platform.OS === 'android' ? (<Text style={{ fontFamily: 'open-sans-bold'}}>Favorites</Text>) : ('Favorites')
         }
     }
 };
@@ -78,6 +85,9 @@ const MealsFavTabNavigator =
     })
     : createBottomTabNavigator(tabSScreenConfig, {
         tabBarOptions: {
+            tabStyle: {
+                fontFamily: 'open-sans'
+            },
             activeTintColor: Colors.accentColor
         }         
      }
